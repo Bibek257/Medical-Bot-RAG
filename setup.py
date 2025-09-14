588f0124b63cba49db508cd4773e1dfa66caf5e6
Bibek <gbibek257@gmail.com>
2025-09-14 21:27:55 +0100

    template,setup and notebook

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical-Chatbot",
+    version="0.0.1",
+    author="Bibek Ghimire",
+    author_email="gbibek257@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
