f50ff2f0af5076237d24c9a6d2ef048fa87a7d3a
Bibek <gbibek257@gmail.com>
2025-09-14 17:49:12 +0100

    file created

---
+++ template.sh
@@ -0,0 +1,20 @@
+# make directories
+mkdir -p src
+mkdir -p research
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+# other files
+touch requirements.txt
+touch README.md
+touch setup.py
+touch app.py
+touch .env
+touch research/trails.ipynb
+
+echo "All files created"
+
+# note--> to run this file type sh template.sh in git bash
